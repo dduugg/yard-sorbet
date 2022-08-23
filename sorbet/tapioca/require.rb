96967344da10b2ab722eaf56f31348423e2e29bf
Douglas Eichelberger <dduugg@users.noreply.github.com>
2022-08-23 12:48:24 -0700

    Use Tapioca for RBI generation (#103)
    * Add tapioca as develoment dependency
    
    * Remove current RBIs
    
    * tapioca init
    
    * rubocop -a
    
    * Add additional requires
    
    * bin/tapioca gem bundler-audit
    
    * bin/tapioca gem rspec-core
    
    * bin/tapioca gem rubocop
    
    * rm rbi task
    
    * Downgrade Bundler version due to bug
    
    Co-authored-by: Kaan Ozkan <kaan.ozkan@shopify.com>

---
+++ sorbet/tapioca/require.rb
@@ -0,0 +1,7 @@
+# typed: strong
+# frozen_string_literal: true
+
+# Add your extra requires here (`bin/tapioca require` can be used to boostrap this list)
+require 'bundler/audit/task'
+require 'rspec/core/rake_task'
+require 'rubocop/rake_task'
